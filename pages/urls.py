7c410d9753a4b3b2cc0f97a3b0eda7c9f0a170c8
sundeep <sunguru98@yahoo.co.in>
2018-12-06 01:32:23 +0530

    Added the urls in pages and created templates and partials for navbar topbar and footer

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.IndexPageTemplateView.as_view(), name="home"),
+    path("", views.IndexPageTemplateView.as_view(), name="index"),
     path("about/", views.AboutPageTemplateView.as_view(), name="about"),
 ]
